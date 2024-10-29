4cf363bdd957a524973e6f2ac25c70ef29219027
Gandalf12042007 <136653696+Gandalf12042007@users.noreply.github.com>
2024-10-29 00:24:43 +0000

    comentario

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int idade;
+    cout << "Digite sua idade: ";
+    cin >> idade;
+    if (idade >= 18) {
+        cout << "Você pode ser preso";
+    } else {
+        cout << "Você vai pra FASE";
+    }
+    return 0;
+}
